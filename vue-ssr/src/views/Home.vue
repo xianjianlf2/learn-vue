8a0abd8a0a3d978c627f1bd2cdaf1d255c96c1dd
La2yTibers <xotoot@vip.qq.com>
2021-09-22 17:10:40 +0800

    update ssr同构

---
+++ vue-ssr/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h2 @click="$store.commit('add')">{{ $store.state.count }}</h2>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+  },
+  asyncData({ store, route }) {
+    return store.dispatch("getCount");
+  },
+};
+</script>
